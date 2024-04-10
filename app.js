e373c9f2677725ee843c1af8620bff6da77d7e6a
Felipe Lemos <>
2024-04-10 11:15:48 +0200

    Refactor

--- app.js
+++ app.js
@@ -39,82 +39,27 @@ const applyCategoryRules = async (
       categories: responseData.masterData?.current?.categories,
     };
   });
+
   let errorFound = false;
-  const result = await Promise.all(fetchPromises)
-    .then((promises) => {
-      if (criteria === "quantity") {
-        //NEED TO CHECK QTY OF THAT LINE ITEM:
-        console.log("Category Quantity Validation");
-        let lineQty = 0;
-        promises.forEach((promise) => {
-          promise.categories.forEach((category) => {
-            if (category.id === wantedCategoryId) {
-              lineQty += promise.lineItem.quantity;
-            }
-          });
-        });
+  const promises = await Promise.all(fetchPromises);
 
-        console.log("Category Quantity Validation - Line Item qty:");
-        console.log("Max value: ", totalValue);
-        console.log("LineItem Qty:", lineQty);
-        console.log(lineQty > totalValue);
-
+  promises.forEach((promise) => {
+    promise.categories.forEach((category) => {
+      if (category.id === wantedCategoryId) {
+        const lineQty = promise.lineItem.quantity;
         if (lineQty > totalValue) {
           errorFound = true;
         }
-
-        return errorFound;
-        /**
-        const categoriesForFlatten = promises.map((promise) => {
-          if (promise.categories.length > 0) {
-            return promise.categories;
-          }
-        });
-
-        const flatCategories = categoriesForFlatten.flat();
-
-        let categoryIds = flatCategories.map((category) => {
-          if (category) {
-            return category.id;
-          }
-        });
-
-        categoryIds = categoryIds.filter(Boolean);
-
-        let categoryIdsAndCounts = categoryIds?.reduce(function (
-          value,
-          value2
-        ) {
-          return value[value2] ? ++value[value2] : (value[value2] = 1), value;
-        },
-        {});
-
-        if (!errorFound) {
-          console.log("Category Quantity Validation - Line Items count:");
-          console.log("Max value count: ", totalValue);
-          console.log(
-            "LineItem Qty count:",
-            categoryIdsAndCounts[wantedCategoryId]
-          );
-          console.log(categoryIdsAndCounts[wantedCategoryId] > totalValue);
-          if (parseInt(categoryIdsAndCounts[wantedCategoryId]) > totalValue) {
-            errorFound = true;
-          }
-        }
-      } */
       }
-    })
-    .catch((error) => {
-      console.error("Error fetching categories:", error);
     });
+  });
 
-  console.log("outside: error found status: ", result);
-  return result;
+  console.log("Error found?", errorFound);
+  return errorFound;
 };
 
 const applySKURules = (lineItems, sku, criteria, totalValue) => {
   let count = 0;
-
   let value = null;
 
   lineItems.forEach((lineItem) => {
@@ -141,8 +86,6 @@ app.post("/ct-cart", async (req, res) => {
   const lineItems = cart.lineItems;
 
   const totalPrice = cart.totalPrice.centAmount / 100;
-
-  //console.log(cart);
 
   const customerGroupKey = await fetchCt(
     `customers/${customerId}?expand=customerGroup`,
@@ -186,7 +129,7 @@ app.post("/ct-cart", async (req, res) => {
       "600388e2-0976-493e-929d-91800b0b3207"
     )
   ) {
-    res.status(400).json({
+    return res.status(400).json({
       errors: [
         {
           code: "InvalidInput",
@@ -196,9 +139,9 @@ app.post("/ct-cart", async (req, res) => {
     });
   }
 
+  let productErrorFound = false;
   if (!errorFound) {
-    let productErrorFound = false;
-    productRules.forEach((rule) => {
+    for (const rule of productRules) {
       if (!productErrorFound) {
         ruleFlag = rule;
         if (rule.type === "sku") {
@@ -211,14 +154,13 @@ app.post("/ct-cart", async (req, res) => {
         }
         if (rule.type === "category") {
           console.log("Category validation:");
-          console.log(rule);
           ruleFlag = {
             type: rule.type,
             value: rule.value,
             criteria: rule.criteria,
             equals: rule.equals.categoryName["en-US"],
           };
-          productErrorFound = applyCategoryRules(
+          productErrorFound = await applyCategoryRules(
             lineItems,
             rule.equals.categoryId,
             rule.criteria,
@@ -226,9 +168,9 @@ app.post("/ct-cart", async (req, res) => {
           );
         }
       }
-    });
-    errorFound = productErrorFound;
+    }
   }
+  errorFound = productErrorFound;
 
   if (errorFound) {
     return res.status(400).json({
@@ -236,12 +178,15 @@ app.post("/ct-cart", async (req, res) => {
         {
           code: "InvalidInput",
           message: `The maximum total ${ruleFlag.criteria} allowed ${
-            ruleFlag.type ? `for ${ruleFlag.type} = ${ruleFlag.equals}` : `cart`
+            ruleFlag.type
+              ? `for ${ruleFlag.type} = ${ruleFlag.equals}`
+              : `for cart`
           } has been exceeded.`,
         },
       ],
     });
   }
+
   return res.status(200).end();
 });
 
